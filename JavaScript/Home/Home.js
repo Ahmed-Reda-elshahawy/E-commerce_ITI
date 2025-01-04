0e641c3f40715302f4c4fa48e649f659f019eb46
yasmin <yasminrabea06@gmail.com>
2025-01-04 21:16:52 +0200

    products functions

--- JavaScript/Home/Home.js
+++ JavaScript/Home/Home.js
@@ -3,3 +3,83 @@ document.querySelectorAll(".btn").forEach(function (btn) {
         btn.classList.toggle("rotated");
     });
 });
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const storedProducts = localStorage.getItem("products");
+//     const products = JSON.parse(storedProducts);
+
+//     if (products) {
+//         const cardContainer = document.getElementById("cardContainerAll");
+
+//         products.forEach(product => {
+//             const cardCol = document.createElement("div");
+//             cardCol.className = "col-6 col-md-4 col-lg-2";
+//             cardCol.innerHTML = `
+//                 <div class="card">
+//                     <div class="card-image-container">
+//                         <img src="${product.images[0]}" class="card-img-top mainimg" alt="${product.title}">
+//                         <div class="add-to-cart-icon">
+//                             <i class="fas fa-cart-plus"></i>
+//                         </div>
+//                     </div>
+//                     <div class="card-body">
+//                         <h5 class="card-title">${product.title}</h5>
+//                         <p class="card-text">${product.tittle2}</p>
+//                     </div>
+//                 </div>
+//             `;
+
+//             cardContainer.appendChild(cardCol);
+//         });
+//     } else {
+//         console.log("No Data");
+//     }
+// });
+
+
+// async function loadDataFromJson() {
+//     try {
+//         const response = await fetch('../../Data/products.json');
+//         const data = await response.json();
+//         localStorage.setItem("products", JSON.stringify(data));
+//         console.log("Data loaded into localStorage:", data);
+//         // return "Products loaded into localStorage";
+//     } catch (error) {
+//         console.error("Error loading data: ", error);
+//         return null;
+//     }
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const storedProducts = localStorage.getItem("products");
+    const products = JSON.parse(storedProducts);
+
+    if (products) {
+        const cardContainer = document.getElementById("cardContainerAll");
+
+        for (let i = 0; i < 8; i++) {
+            const product = products[i];
+            const cardCol = document.createElement("div");
+            cardCol.className = "col-6 col-md-4 col-lg-2";
+            cardCol.innerHTML += `
+                <div class="card">
+                    <div class="card-image-container">
+                        <img src="${product.images[0]}" class="card-img-top mainimg" alt="${product.title}">
+                        <div class="add-to-cart-icon">
+                            <i class="fas fa-cart-plus"></i>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">${product.title}</h5>
+                        <p class="card-text">${product.tittle2}</p>
+                    </div>
+                </div>
+            `;
+
+            cardContainer.appendChild(cardCol);
+        }
+    } else {
+        console.log("No Data");
+    }
+});
