be3c7c7574384f73c6f49b4fb532a08fbdffa50f
yasmin <yasminrabea06@gmail.com>
2025-01-05 01:19:08 +0200

    dynamic data and some other pages

--- JavaScript/Home/Home.js
+++ JavaScript/Home/Home.js
@@ -4,66 +4,28 @@ document.querySelectorAll(".btn").forEach(function (btn) {
     });
 });
 
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const storedProducts = localStorage.getItem("products");
-//     const products = JSON.parse(storedProducts);
-
-//     if (products) {
-//         const cardContainer = document.getElementById("cardContainerAll");
-
-//         products.forEach(product => {
-//             const cardCol = document.createElement("div");
-//             cardCol.className = "col-6 col-md-4 col-lg-2";
-//             cardCol.innerHTML = `
-//                 <div class="card">
-//                     <div class="card-image-container">
-//                         <img src="${product.images[0]}" class="card-img-top mainimg" alt="${product.title}">
-//                         <div class="add-to-cart-icon">
-//                             <i class="fas fa-cart-plus"></i>
-//                         </div>
-//                     </div>
-//                     <div class="card-body">
-//                         <h5 class="card-title">${product.title}</h5>
-//                         <p class="card-text">${product.tittle2}</p>
-//                     </div>
-//                 </div>
-//             `;
-
-//             cardContainer.appendChild(cardCol);
-//         });
-//     } else {
-//         console.log("No Data");
-//     }
-// });
-
-
-// async function loadDataFromJson() {
-//     try {
-//         const response = await fetch('../../Data/products.json');
-//         const data = await response.json();
-//         localStorage.setItem("products", JSON.stringify(data));
-//         console.log("Data loaded into localStorage:", data);
-//         // return "Products loaded into localStorage";
-//     } catch (error) {
-//         console.error("Error loading data: ", error);
-//         return null;
-//     }
-// }
-
 document.addEventListener("DOMContentLoaded", () => {
-    const storedProducts = localStorage.getItem("products");
-    const products = JSON.parse(storedProducts);
+    let products = JSON.parse(localStorage.getItem("products"));
 
-    if (products) {
+    if (products && products.products) {
         const cardContainer = document.getElementById("cardContainerAll");
 
-        for (let i = 0; i < 8; i++) {
-            const product = products[i];
+        let shuffledProducts = JSON.parse(localStorage.getItem("shuffledProducts"));
+
+        if (!shuffledProducts) {
+            shuffledProducts = products.products.sort(() => Math.random() - 0.5);
+
+            localStorage.setItem("shuffledProducts", JSON.stringify(shuffledProducts));
+        }
+
+        console.log("Stored products:", shuffledProducts);
+
+        for (let i = 0; i < Math.min(12, shuffledProducts.length); i++) {
+            const product = shuffledProducts[i];
             const cardCol = document.createElement("div");
             cardCol.className = "col-6 col-md-4 col-lg-2";
-            cardCol.innerHTML += `
-                <div class="card">
+            cardCol.innerHTML = `
+                <div class="card w-100 h-100">
                     <div class="card-image-container">
                         <img src="${product.images[0]}" class="card-img-top mainimg" alt="${product.title}">
                         <div class="add-to-cart-icon">
@@ -71,15 +33,60 @@ document.addEventListener("DOMContentLoaded", () => {
                         </div>
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">${product.title}</h5>
-                        <p class="card-text">${product.tittle2}</p>
+                        <h6 class="card-title"><b>${product.title}</b></h6>
+                        <p class="itemPrice">${product.price}$</p>
+                    </div>
+                </div>
+            `;
+            cardContainer.appendChild(cardCol);
+        }
+    } else {
+        console.log("No products available or invalid data.");
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const storedProducts = localStorage.getItem("products");
+    const products = JSON.parse(storedProducts);
+
+    if (products) {
+        const cardContainerSale = document.getElementById("cardContainerAllSale");
+
+        for (let i = 30; i < 36; i++) {
+            const product = products[i];
+            const cardCol = document.createElement("div");
+            cardCol.className = "col-6 col-md-4 col-lg-2";
+            cardCol.innerHTML += `
+                <div class="card w-100 h-100">
+                    <div class="card-image-container">
+                        <img src="${products.products[i].images[0]}" class="card-img-top mainimg" alt="${products.products[i].title}">
+                        <div class="add-to-cart-icon">
+                            <i class="fas fa-cart-plus"></i>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">${products.products[i].title}</h5>
+                        <p class="itemPrice"><del style="color: rgb(85, 84, 84)">${products.products[i].price}$</del> ${((products.products[i].price)-((products.products[i].price)*((products.products[i].discountPercentage)/100))).toFixed(2)}$</p>
                     </div>
                 </div>
             `;
 
-            cardContainer.appendChild(cardCol);
+            cardContainerSale.appendChild(cardCol);
         }
     } else {
         console.log("No Data");
     }
 });
+
+document.getElementById("NewArrivalButton").addEventListener("click", newArrival);
+
+function newArrival(){
+    window.location.href = "../Home/NewArrival.html"
+}
+
+document.getElementById("shopNowButton").addEventListener("click", shopNow);
+
+function shopNow(){
+    window.location.href = "../Home/ShopAll.html"
+}
